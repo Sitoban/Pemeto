68841a8f6458e42d814a0c6f793561ecd334b80b
Sitoban Biswas <sitoban.biswas@jda.com>
2016-08-17 22:14:43 +0530

    FirstCheck

---
+++ background.js
@@ -0,0 +1,53 @@
+var statusType = {
+    IDLE: 0,
+    START: 1,
+    STOP: 2
+}
+
+var currentStatus = statusType.IDLE;
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+    if (currentStatus != statusType.START) {
+        currentStatus = statusType.START;
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "start_recording" }, function (response) {
+
+            });
+        });
+        chrome.browserAction.setBadgeText({ text: '►' });
+    }
+    else {
+        this._stopRecording();
+    }
+});
+
+chrome.commands.onCommand.addListener(function (command) {
+    console.log('Command:', command);
+    if (command == "stop_recording" && currentStatus == statusType.START) {
+        this._stopRecording();
+    }
+
+});
+
+function _stopRecording()
+{
+    currentStatus = statusType.STOP;
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "stop_recording" });
+
+    });
+    chrome.browserAction.setBadgeText({ text: '' });
+}
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log('Got the message');
+
+    });
+
+
+
+
+
+
---
+++ content_script.js
@@ -0,0 +1,65 @@
+
+var Timing = {
+  StartTime: null,
+  EndTime: null
+}
+
+var statusType = {
+  IDLE: 0,
+  RUNNING: 1
+}
+
+var currentStatus = statusType.IDLE;
+
+function dOMSubtreeModified() {
+
+  if (currentStatus == statusType.IDLE)
+  { 
+    Timing.StartTime = new Date(); 
+    console.log("StartTime : "+Timing.StartTime);
+    currentStatus = statusType.RUNNING
+  }
+  else
+  {
+    Timing.EndTime = new Date();
+    //console.log("EndTime : "+Timing.EndTime);
+  }
+  // console.log('DOM Changed');
+  //chrome.runtime.sendMessage("Hello Sito!");
+}
+
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.action == 'start_recording') {
+      console.log('Attaching Event Listener..');
+      document.body.addEventListener('DOMSubtreeModified', dOMSubtreeModified, false);
+    }
+    else if (currentStatus == statusType.RUNNING)
+    {
+      document.body.removeEventListener('DOMSubtreeModified', dOMSubtreeModified, false);
+      console.log('Removing Event Listener..');
+      logDifferece(Timing.StartTime,Timing.EndTime);
+      currentStatus = statusType.IDLE;
+    }
+  });
+
+function logDifferece(date1, date2) {
+
+  var diff = (date2 - date1) / 1000;
+  var milisec = (date2 - date1) % 1000
+  var diff = Math.abs(Math.floor(diff));
+
+  var days = Math.floor(diff / (24 * 60 * 60));
+  var leftSec = diff - days * 24 * 60 * 60;
+
+  var hrs = Math.floor(leftSec / (60 * 60));
+  var leftSec = leftSec - hrs * 60 * 60;
+
+  var min = Math.floor(leftSec / (60));
+  var leftSec = leftSec - min * 60;
+
+
+  console.log(hrs + " hours " + min + " minutes and " + leftSec + " seconds with "+milisec+" millisecond.");
+
+}
